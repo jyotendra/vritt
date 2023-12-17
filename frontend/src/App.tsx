af5f11614189622d2e444185c440eaeeee24c03c
jyotendra <jyotendrasharma92@gmail.com>
2023-12-17 14:53:36 +0530

    added mui dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// material ui font imports
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 function App() {
   const [count, setCount] = useState(0)
 
