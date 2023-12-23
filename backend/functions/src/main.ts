1d1a0a43082731c0fa5d4caa1886debd739e89d8
jyotendra <jyotendrasharma92@gmail.com>
2023-12-23 16:26:42 +0530

    added cloud function support

--- backend/functions/src/main.ts
+++ backend/functions/src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import * as express from "express";
+import { onRequest } from "firebase-functions/v2/https";
+import { AppModule } from "./app.module";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const expressServer = express();
+const createFunction = async (expressInstance): Promise<void> => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  await app.init();
+};
+export const api = onRequest(async (request, response) => {
+  await createFunction(expressServer);
+  expressServer(request, response);
+});
